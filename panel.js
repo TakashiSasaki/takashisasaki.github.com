7d38ae7bc299d21093edfd79e7bd8e760f538b9d
Takashi SASAKI <takashi316@gmail.com>
2014-04-09 15:16:25 +0900

    共通パネル表示バグ修正

--- panel.js
+++ panel.js
@@ -1,15 +1,18 @@
 $(document).one("pagebeforecreate", function (e, ui) {
 
-    var panel = "<div data-role='panel' id='panel'>";
+    var panel = "";
     panel += '<ul data-role="listview">';
     panel += '<li><a href="index.html">ホーム</a></li>';
     panel += '<li data-role="list-divider">実験</li>';
-    panel += '<li><a href="google-oauth2-js.html#installed-apps">OAuth2 for Install Apps</a></li>';
+    panel += '<li><a href="google-oauth2-js.html#installed-apps">OAuth2 for Installed Apps</a></li>';
     panel += '<li><a href="google-oauth2-js.html#client-side">OAuth2 for Client-side</a></li>';
     panel += '<li><a href="google-oauth2-js.html#devices">OAuth2 for Devices</a></li>';
     panel += '</ul>';
-    panel += "</div>";
-    $.mobile.pageContainer.prepend(panel);
+    panel += "";
+    //$.mobile.pageContainer.append(panel);
+    //$.mobile.pageContainer.prepend(panel);
+    $("#panel").append(panel);
+    //$("body").append(panel);
     $("#panel").panel();
     $("#panel ul").listview();
 });
