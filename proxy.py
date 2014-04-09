9fc3f3ff3fb0c6937d55f3709ce0e25bd369a811
Takashi SASAKI <takashi316@gmail.com>
2014-04-09 21:44:46 +0900

    proxy_post実装中

--- proxy.py
+++ proxy.py
@@ -3,6 +3,9 @@ import http.server
 import logging
 import cgi
 import json
+import urllib.parse
+import http.client
+import ssl
 
 
 def do_GET(self):
@@ -14,8 +17,16 @@ def do_GET(self):
     <html>
     <head></head>
     <body><form action="/proxy" method="POST">
-
-    <textarea name="ajax" placeholder="{}" cols="60" rows="10"></textarea>
+    <textarea name="ajax_option" cols="60" rows="10">
+        {
+            "type": "get",
+            "url":"http://localhost?a=b#c=d",
+            "data": {
+                "param1":"value1",
+                "param2":"value2"
+            }
+        }
+    </textarea>
     <button type="submit">submit</button>
     </form>
     <script></script>
@@ -36,8 +47,34 @@ def do_POST(self):
     logging.info(self.headers["Content-Type"])
     body = self.rfile.read(body_len)
     decoded_body = cgi.parse_qs(body)
-    json_bytes = decoded_body[b"ajax"][0]
+    json_bytes = decoded_body[b"ajax_option"][0]
     json_string = json_bytes.decode("UTF-8")
     logging.info(json_string)
     decoded_json = json.loads(json_string)
+    proxy_post(self, decoded_json)
     self.wfile.write(bytes(decoded_json["url"], "UTF-8"))
+
+
+def proxy_post(self, ajax_option):
+    parsed_url = urllib.parse.urlparse(ajax_option["url"])
+    scheme = parsed_url.scheme
+    netloc = parsed_url.netloc
+    path = parsed_url.path
+    params = parsed_url.params
+    query = parsed_url.query
+    fragment = parsed_url.fragment
+    username = parsed_url.username
+    password = parsed_url.password
+    hostname = parsed_url.hostname
+    port = parsed_url.port
+    if scheme == "http" or scheme == "HTTP":
+        if port is None:
+            port = 80
+        http_connection = http.client.HTTPConnection(netloc, port)
+    if scheme == "https" or scheme == "HTTPS":
+        if port is None:
+            port = 443
+        context = ssl.create_default_context()
+        http_connection = http.client.HTTPConnection(netloc, port, context=context)
+
+    http_connection.request(ajax_option.type, "/" + ajax_option.path)
