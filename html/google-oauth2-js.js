2c80828f9497988cda736f03b2bf9131effd1237
Takashi SASAKI <takashi316@gmail.com>
2014-04-11 16:20:54 +0900

    テスト用の簡単な構造のページ

---
+++ html/google-oauth2-js.js
@@ -0,0 +1,3 @@
+/**
+ * Created by sasaki on 14/04/11.
+ */
---
+++ html/test.js
@@ -0,0 +1,5 @@
+$("#right-panel ul").empty();
+$("#right-panel ul").append('<li><a href="#page-test-1">page 1</a></li>');
+$("#right-panel ul").append('<li><a href="#page-test-2">page 2</a></li>');
+$("#right-panel ul").append('<li><a href="#page-test-3">page 3</a></li>');
+$("#right-panel ul").listview().listview("refresh");
