95833085976b85b3217e8f18676b0aee48ebbd72
Takashi SASAKI <takashi316@gmail.com>
2014-04-14 20:11:49 +0900

    パネル内のメインメニューを静的にした

--- panel.js
+++ panel.js
@@ -1,14 +1,5 @@
 $(document).one("pagecreate", function (e, ui) {
     //alert("pagebeforecreate");
-    $("#left-panel ul").append('<li data-role="list-divider">めにう</li>');
-    $("#left-panel ul").append('<li><a href="#index">ホーム</a></li>');
-    $("#left-panel ul").append('<li><a href="#appcache">アプリケーションキャッシュ</a></li>');
-    $("#left-panel ul").append('<li><a href="html/google-oauth2-client-side.html">OAuth2 for JavaScript Apps</a></li>');
-    $("#left-panel ul").append('<li><a href="html/google-oauth2-installed-apps.html">OAuth2 for Installed Apps</a></li>');
-    $("#left-panel ul").append('<li><a href="html/google-oauth2-devices.html">OAuth2 for Devices</a></li>');
-    $("#left-panel ul").append('<li><a href="html/google-drive.html">Google Drive API Demo</a></li>');
-    $("#left-panel ul").append('<li><a href="html/offline-content.html">Offline Content Demo</a></li>');
-    $("#left-panel ul").append('<li><a href="html/test.html">test</a></li>');
     //$.mobile.pageContainer.append(panel);
     //$.mobile.pageContainer.prepend(panel);
     //$("body").append(panel)
