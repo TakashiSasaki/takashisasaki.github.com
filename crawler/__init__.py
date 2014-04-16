0a8e74a6d829ee72cb2df13862c61820eb51b011
Takashi SASAKI <takashi316@gmail.com>
2014-04-16 16:57:11 +0900

    /crawlerのハンドラ書き始め

--- crawler/__init__.py
+++ crawler/__init__.py
@@ -1,1 +1,14 @@
-__author__ = 'sasaki'
+__author__ = 'Takashi SASAKI'
+import http.server
+
+
+def do_POST(self):
+    assert isinstance(self, http.server.BaseHTTPRequestHandler)
+    self.wfile.write(bytes("do_GET", "UTF-8"))
+    pass
+
+
+def do_GET(self):
+    assert isinstance(self, http.server.BaseHTTPRequestHandler)
+    self.wfile.write(bytes("do_GET", "UTF-8"))
+    pass
